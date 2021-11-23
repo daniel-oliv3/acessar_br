ab6a2106b38fa07cf744e24941af5b01895eff79
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2021-11-22 23:09:22 -0300

    Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import react from "react";
+import './components/Header';
 import './styles/global.scss';
 
 function App() {
   return (
     <div>
       Olá, Mundo!
+      
     </div>
   );
 }
---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import react from "react";
+//import 'styles';
+
+const Header = () => {
+  return (
+    <header className="header__container">
+      <div className="header__logo--state">
+
+      </div>
+      <div className="header__logo--">
+
+      </div>
+    </header>
+  );
+}
+
